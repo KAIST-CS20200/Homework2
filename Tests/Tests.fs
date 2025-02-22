2d7aa1da105edd15cfab964340f245e059ffd582
Sang Kil Cha <sangkilc@kaist.ac.kr>
2025-02-22 23:05:32 +0900

    Use .NET 9

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -7,88 +7,88 @@ type TestClass () =
 
   [<TestMethod; Timeout 1000; TestCategory "1">]
   member __.``Problem 1.A``() =
-    let r = Program.prob1 30
-    Assert.AreEqual (4, r)
+    let r: int = Program.prob1 30
+    Assert.AreEqual<int> (4, r)
 
   [<TestMethod; Timeout 1000; TestCategory "1">]
   member __.``Problem 1.B``() =
-    let r = Program.prob1 328
-    Assert.AreEqual (218, r)
+    let r: int = Program.prob1 328
+    Assert.AreEqual<int> (218, r)
 
   [<TestMethod; Timeout 1000; TestCategory "1">]
   member __.``Problem 1.C``() =
-    let r = Program.prob1 1337
-    Assert.AreEqual (12207, r)
+    let r: int = Program.prob1 1337
+    Assert.AreEqual<int> (12207, r)
 
   [<TestMethod; Timeout 1000; TestCategory "1">]
   member __.``Problem 1.D``() =
-    let r = Program.prob1 -1000
-    Assert.AreEqual (-1, r)
+    let r: int = Program.prob1 -1000
+    Assert.AreEqual<int> (-1, r)
 
   [<TestMethod; Timeout 1000; TestCategory "2">]
   member __.``Problem 2.A``() =
-    let r = Program.prob2 59129123 1929149
-    Assert.AreEqual (1, r)
+    let r: int = Program.prob2 59129123 1929149
+    Assert.AreEqual<int> (1, r)
 
   [<TestMethod; Timeout 1000; TestCategory "2">]
   member __.``Problem 2.B``() =
-    let r = Program.prob2 10502050 4012032
-    Assert.AreEqual (2, r)
+    let r: int = Program.prob2 10502050 4012032
+    Assert.AreEqual<int> (2, r)
 
   [<TestMethod; Timeout 1000; TestCategory "2">]
   member __.``Problem 2.C``() =
-    let r = Program.prob2 -1 -1
-    Assert.AreEqual (1, r)
+    let r: int = Program.prob2 -1 -1
+    Assert.AreEqual<int> (1, r)
 
   [<TestMethod; Timeout 1000; TestCategory "2">]
   member __.``Problem 2.D``() =
-    let r = Program.prob2 10 0
-    Assert.AreEqual (10, r)
+    let r: int = Program.prob2 10 0
+    Assert.AreEqual<int> (10, r)
 
   [<TestMethod; Timeout 1000; TestCategory "2">]
   member __.``Problem 2.E``() =
-    let r = Program.prob2 0 0
-    Assert.AreEqual (-1, r)
+    let r: int = Program.prob2 0 0
+    Assert.AreEqual<int> (-1, r)
 
   [<TestMethod; Timeout 1000; TestCategory "3">]
   member __.``Problem 3.A``() =
-    let r = Program.prob3 "cs220" 0
-    Assert.AreEqual ("", r)
+    let r: string = Program.prob3 "cs220" 0
+    Assert.AreEqual<string> ("", r)
 
   [<TestMethod; Timeout 1000; TestCategory "3">]
   member __.``Problem 3.B``() =
-    let r = Program.prob3 "cs220" -1
-    Assert.AreEqual ("022sc", r)
+    let r: string = Program.prob3 "cs220" -1
+    Assert.AreEqual<string> ("022sc", r)
 
   [<TestMethod; Timeout 1000; TestCategory "3">]
   member __.``Problem 3.C``() =
-    let r = Program.prob3 "cs220" 5
-    Assert.AreEqual ("cs220cs220cs220cs220cs220", r)
+    let r: string = Program.prob3 "cs220" 5
+    Assert.AreEqual<string> ("cs220cs220cs220cs220cs220", r)
 
   [<TestMethod; Timeout 1000; TestCategory "3">]
   member __.``Problem 3.D``() =
-    let r = Program.prob3 "abcde" -5
-    Assert.AreEqual ("edcbaedcbaedcbaedcbaedcba", r)
+    let r: string = Program.prob3 "abcde" -5
+    Assert.AreEqual<string> ("edcbaedcbaedcbaedcbaedcba", r)
 
   [<TestMethod; Timeout 1000; TestCategory "4">]
   member __.``Problem 4.A``() =
-    let r = Program.prob4 0u
-    Assert.AreEqual (0u, r)
+    let r: uint32 = Program.prob4 0u
+    Assert.AreEqual<uint32> (0u, r)
 
   [<TestMethod; Timeout 1000; TestCategory "4">]
   member __.``Problem 4.B``() =
-    let r = Program.prob4 120u
-    Assert.AreEqual (2u, r)
+    let r: uint32 = Program.prob4 120u
+    Assert.AreEqual<uint32> (2u, r)
 
   [<TestMethod; Timeout 1000; TestCategory "4">]
   member __.``Problem 4.C``() =
-    let r = Program.prob4 65537u
-    Assert.AreEqual (65537u, r)
+    let r: uint32 = Program.prob4 65537u
+    Assert.AreEqual<uint32> (65537u, r)
 
   [<TestMethod; Timeout 1000; TestCategory "4">]
   member __.``Problem 4.D``() =
-    let r = Program.prob4 1688611u
-    Assert.AreEqual (401u, r)
+    let r: uint32 = Program.prob4 1688611u
+    Assert.AreEqual<uint32> (401u, r)
 
   [<TestMethod; Timeout 1000; TestCategory "5">]
   member __.``Problem 5.A``() =
